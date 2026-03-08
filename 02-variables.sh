fca85b4ce7bfab46c8e3881e3f57749533738fad
Rakesh Thakkellapally <rakesh.athit@gmail.com>
2026-03-08 17:03:54 +0530

    Variable names changed

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-person1=Ramesh
-person2=Suresh
+person1=trump
+person2=putin
 echo "$person1::Hello $person2 How are you"
 echo "$person2::Hello $person1 iam fine how about you?"
 echo "$person1::Am too good $person2 Thanks!"

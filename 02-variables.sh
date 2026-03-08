e8f986a5e54224a35c1eaffc547d9cb440e0da9e
Rakesh Thakkellapally <rakesh.athit@gmail.com>
2026-03-08 17:02:27 +0530

    variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-person1=$person1
-person2=$person2
+person1=Ramesh
+person2=Suresh
 echo "$person1::Hello $person2 How are you"
 echo "$person2::Hello $person1 iam fine how about you?"
 echo "$person1::Am too good $person2 Thanks!"

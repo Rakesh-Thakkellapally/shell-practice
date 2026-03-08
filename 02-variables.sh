b6781f416da6c04ea0b93208c542b024b73e3026
Rakesh Thakkellapally <rakesh.athit@gmail.com>
2026-03-08 17:05:26 +0530

    passing variables dynamically

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-person1=trump
-person2=putin
+person1=$1
+person2=$2
 echo "$person1::Hello $person2 How are you"
 echo "$person2::Hello $person1 iam fine how about you?"
 echo "$person1::Am too good $person2 Thanks!"

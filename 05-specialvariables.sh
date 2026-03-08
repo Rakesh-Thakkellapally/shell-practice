84d22fe7681060cd4e4168f3aad8c513c29f31ad
Rakesh Thakkellapally <rakesh.athit@gmail.com>
2026-03-08 18:12:12 +0530

    special variables

--- 05-specialvariables.sh
+++ 05-specialvariables.sh
@@ -6,6 +6,6 @@ echo "present working directory:$PWD"
 echo "who is running:$USER"
 echo "home directory of current user:$HOME"
 echo "PID of the script:$$"
-sleep 10&
+
 echo "PUD of recently executed background process:$!"
 echo all args passed to script:$*"

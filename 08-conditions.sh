d24961489d6380eff4ee5519792bc3110e855065
Rakesh Thakkellapally <rakesh.athit@gmail.com>
2026-03-08 18:52:57 +0530

    conditions

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 number=$1
-if [ $number -gt 50 ];then
-echo "given number:$number is greater than 50"
-elif[ $number -lt 50 ];then
-echo "given number $number is less than 50"
+if [ $number -gt 50 ]; then
+    echo "given number:$number is greater than 50"
+elif [ $number -lt 50 ]; then
+    echo "given number $number is less than 50"
 else
-echo "given number $number is equal to 50"
+    echo "given number $number is equal to 50"
 fi

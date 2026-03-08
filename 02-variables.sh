97b9d9c6af25b43c35f63f2551b4d21fb92177c0
Rakesh Thakkellapally <rakesh.athit@gmail.com>
2026-03-08 16:53:33 +0530

    variables

---
+++ 02-variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Suresh::Hello Ramesh How are you"
+echo "Ramesh::Hello Suresh iam fine how about you?"
+echo "Suresh::Am too good Ramesh Thanks!"
+echo "Ramesh::Am learning DevOps Suresh"

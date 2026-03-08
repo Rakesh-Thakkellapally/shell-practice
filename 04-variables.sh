c9fbf07f0212fa183f4030e86c57b4405ffac04d
Rakesh Thakkellapally <rakesh.athit@gmail.com>
2026-03-08 17:36:52 +0530

    time stamp

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 StartTime=$(date +"%Y-%m-%d_%H-%M-%S")
 echo "script executed start time is :$StartTime"
+sleep 10
 endTime=$(date +"%Y-%m-%d_%H-%M-%S")
 echo "script execution end time is :$endTime"

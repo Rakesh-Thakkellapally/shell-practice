d63c2fce290bb630183fde67281d42214240469c
Rakesh Thakkellapally <rakesh.athit@gmail.com>
2026-03-08 17:41:06 +0530

    time stamp

--- 04-variables.sh
+++ 04-variables.sh
@@ -4,5 +4,5 @@ echo "script executed start time is :$StartTime"
 sleep 10
 endTime=$(date +"%Y-%m-%d_%H-%M-%S")
 echo "script execution end time is :$endTime"
-totaltime=$($endTime-$StartTime)
+totaltime=$(($endTime-$StartTime))
 echo "Total time taken for execution is :$totaltime"

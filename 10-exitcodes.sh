3a59b1951a5af44ac351d2c36965eedd35998331
Rakesh Thakkellapally <rakesh.athit@gmail.com>
2026-03-09 20:02:28 +0530

    exit code

--- 10-exitcodes.sh
+++ 10-exitcodes.sh
@@ -3,11 +3,6 @@ userid=$(id -u)
 if [ $userid -ne 0 ];then
 echo "please run this with root user"
 exit 1
-fi 
-echo "installing nginx"
+else
 dnf install nginx -y
-if [ $? -ne 0 ];then
-echo "installing nginx ..failure"
-else 
-echo "installing nginx success"
 fi

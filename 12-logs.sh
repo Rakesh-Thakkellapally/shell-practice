c120e1cc313537c016b9123c90a542c8d01ba75c
Rakesh Thakkellapally <rakesh.athit@gmail.com>
2026-03-10 19:24:09 +0530

    logs

--- 12-logs.sh
+++ 12-logs.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
-userid=(id -u)
-path="/var/log/shell_script"
-logfile="/var/log/shell_script/$0.log
-if [ $userid -ne 0 ];then
-echo "please run this with root" |tee -a $logfile
-exit 1
-fi
-mkdir -p $path
-validate(){
-if [ $1 -ne 0 ];then
-echo "$2...failure" | tee -a $logfile
-exit 1
-else
-echo "$2...success" | tee -a $logfile
-fi
-}
-dnf install nginx -y &>> "$logfile"
-validate $? "installing nginx"
-dnf install mysql -y &>> "$logfile"
-validate $? "installing mysql"
 
+# Get the user ID
+userid=$(id -u)
+
+# Define log path and logfile
+path="/var/log/shell_script"
+logfile="$path/$(basename $0).log"
+
+# Check if running as root
+if [ "$userid" -ne 0 ]; then
+  echo "Please run this with root" | tee -a "$logfile"
+  exit 1
+fi
+
+# Create log directory
+mkdir -p "$path"
+
+# Validation function
+validate() {
+  if [ $1 -ne 0 ]; then
+    echo "$2...failure" | tee -a "$logfile"
+    exit 1
+  else
+    echo "$2...success" | tee -a "$logfile"
+  fi
+}
+
+# Install nginx
+dnf install nginx -y &>> "$logfile"
+validate $? "Installing nginx"
+
+# Install mysql
+dnf install mysql -y &>> "$logfile"
+validate $? "Installing mysql"

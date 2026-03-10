460a1f8fe4b2292f329d35e3091e648ec3dd00c9
Rakesh Thakkellapally <rakesh.athit@gmail.com>
2026-03-10 19:17:47 +0530

    logs

---
+++ 12-logs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+userid=(id -u)
+path="/var/log/shell_script"
+logfile="/var/log/shell_script/$0.log
+if [ $userid -ne 0 ];then
+echo "please run this with root" |tee -a $logfile
+exit 1
+fi
+mkdir -p $path
+validate(){
+if [ $1 -ne 0 ];then
+echo "$2...failure" | tee -a $logfile
+exit 1
+else
+echo "$2...success" | tee -a $logfile
+fi
+}
+dnf install nginx -y &>> $logfile
+validate $? "installing nginx"
+dnf install mysql -y &>> $logfile
+validate $? "installing mysql"
+
